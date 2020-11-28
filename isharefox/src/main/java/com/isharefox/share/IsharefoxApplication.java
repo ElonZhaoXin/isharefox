5c745d6449fecda3bbbc419156861ad4f52c091c
ElonZhaoXin <64085944+ElonZhaoXin@users.noreply.github.com>
2020-11-28 09:51:18 +0800

    initialize

---
+++ isharefox/src/main/java/com/isharefox/share/IsharefoxApplication.java
@@ -0,0 +1,13 @@
+package com.isharefox.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsharefoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsharefoxApplication.class, args);
+	}
+
+}
