ceadc278bd18a48ac84635044bb8622c9ccf4e82
ElonZhaoXin <64085944+ElonZhaoXin@users.noreply.github.com>
2020-11-28 20:01:03 +0800

    Update createdb.sql

--- isharefox/src/test/resources/sql/createdb.sql
+++ isharefox/src/test/resources/sql/createdb.sql
@@ -1,6 +1,93 @@
--- 用户表 user_info --
+CREATE DATABASE  IF NOT EXISTS `isharefox` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `isharefox`;
+-- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: isharefox
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
--- 客户账户表 user_account --
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- 商品表 item_info --
+--
+-- Table structure for table `item`
+--
 
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `resourceId` varchar(4) NOT NULL COMMENT '资源编号，在支付URL中：http://isharefox.com/share/userId/resourceId',
+  `zippwd` varchar(6) NOT NULL COMMENT '解压密码',
+  `desc` varchar(60) DEFAULT NULL COMMENT '商品描述',
+  `amount` decimal(10,0) NOT NULL COMMENT '收费金额',
+  `userId` varchar(4) NOT NULL COMMENT '客户编号',
+  `status` varchar(1) NOT NULL COMMENT '资源状态，0-创建，未发布；1=上架，正常售卖；2-下架',
+  `createtime` datetime NOT NULL COMMENT '创建时间',
+  `updatetime` datetime NOT NULL COMMENT '最后更新时间',
+  PRIMARY KEY (`id`),
+  KEY `UK` (`resourceId`,`userId`) COMMENT '联合索引，唯一确认一个支付资源情况'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` varchar(4) NOT NULL COMMENT '客户编号',
+  `nickName` varchar(15) NOT NULL COMMENT '昵称',
+  `email` varchar(255) DEFAULT NULL COMMENT '登录邮箱',
+  `pwd` varchar(9) NOT NULL COMMENT '登录密码',
+  `cellPhoneNum` varchar(11) NOT NULL COMMENT '手机号',
+  `status` varchar(1) NOT NULL COMMENT '0-异常；1-正常',
+  `createtime` datetime NOT NULL COMMENT '创建时间',
+  `updatetime` datetime NOT NULL COMMENT '最后更新时间',
+  PRIMARY KEY (`id`),
+  KEY `UK` (`userId`) COMMENT '客户编号唯一'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_account`
+--
+
+DROP TABLE IF EXISTS `user_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_account` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` varchar(4) NOT NULL COMMENT '客户编号',
+  `type` varchar(1) NOT NULL COMMENT '账户类型，0-微信；1-支付宝',
+  `accountNo` varchar(255) NOT NULL COMMENT '1-支付宝：手机号/账号；',
+  `status` varchar(1) NOT NULL COMMENT '0-异常；1-正常',
+  `createtime` datetime NOT NULL COMMENT '创建时间',
+  `updatetime` datetime NOT NULL COMMENT '最后更新时间',
+  PRIMARY KEY (`id`),
+  KEY `UK` (`userId`) COMMENT '客户编号唯一'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-28 19:59:47
